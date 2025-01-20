a34eec4852aad548bb8227879ec57b096472aeea
kyu <knh3136@naver.com>
2025-01-20 09:42:05 +0900

    Navbar 링크

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,11 @@
 import React from "react";
 import "./Navbar.css";
+import rocket from "../../assets/rocket.png";
+import star from "../../assets/glowing-star.png";
+import idButton from "../../assets/id-button.png";
+import memo from "../../assets/memo.png";
+import order from "../../assets/package.png";
+import lock from "../../assets/locked.png";
 
 const Navbar = () => {
   return (
@@ -17,7 +23,11 @@ const Navbar = () => {
           </button>
         </form>
       </div>
-      <div className="align_center navbar_links"></div>
+      <div className="align_center navbar_links">
+        <a href="#" className="align_center">
+          Home <img src={rocket} alt="" className="link_emoji" />
+        </a>
+      </div>
     </nav>
   );
 };
