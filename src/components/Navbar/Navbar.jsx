d51fc3b869cc2b10156fa739afaf5fba6fd90906
kyu <knh3136@naver.com>
2025-01-20 10:23:15 +0900

    리드위드아이콘

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,6 +6,7 @@ import idButton from "../../assets/id-button.png";
 import memo from "../../assets/memo.png";
 import order from "../../assets/package.png";
 import lock from "../../assets/locked.png";
+import LinkWithIcon from "./LinkWithIcon";
 
 const Navbar = () => {
   return (
@@ -24,8 +25,14 @@ const Navbar = () => {
         </form>
       </div>
       <div className="align_center navbar_links">
-        <a href="#" className="align_center">
-          Home <img src={rocket} alt="" className="link_emoji" />
+        <LinkWithIcon title={"홈페이지"} link={"/"} emoji={rocket} />
+        <LinkWithIcon title={"상품들"} link={"/products"} emoji={star} />
+        <LinkWithIcon title={"로그인"} link={"/login"} emoji={idButton} />
+        <LinkWithIcon title={"가입"} link={"/signup"} emoji={memo} />
+        <LinkWithIcon title={"내주문"} link={"/myorders"} emoji={order} />
+        <LinkWithIcon title={"로그아웃"} link={"/logout"} emoji={lock} />
+        <a href="/cart" className="align_center">
+          장바구니 <p className="align_center cart_counts">0</p>
         </a>
       </div>
     </nav>
