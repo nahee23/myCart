80489777cd113bd020962ff02b176ab131fa1f90
kyu <knh3136@naver.com>
2025-01-21 09:33:18 +0900

    useState Form

--- src/components/Authentication/LoginPage.jsx
+++ src/components/Authentication/LoginPage.jsx
@@ -1,16 +1,27 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import "./LoginPage.css";
 
 const LoginPage = () => {
-  const passwordRef = useRef(null);
+  //const passwordRef = useRef(null);
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(user);
+    setUser({ email: "", password: "" }); //초기값으로 변환
+  };
   return (
     <section className="align_center form_page">
-      <form className="authentication_form">
+      <form className="authentication_form" onSubmit={handleSubmit}>
         <h2>로그인 폼</h2>
         <div className="form_inputs">
           <div>
             <label htmlFor="email">Email</label>
             <input
+              onChange={(e) => setUser({ ...user, email: e.target.value })}
+              value={user.email}
               type="email"
               id="email"
               className="form_text_input"
@@ -20,13 +31,15 @@ const LoginPage = () => {
           <div>
             <label htmlFor="password">Password</label>
             <input
+              onChange={(e) => setUser({ ...user, password: e.target.value })}
+              value={user.password}
               type="password"
-              ref={passwordRef}
+              //ref={passwordRef}
               id="password"
               className="form_text_input"
               placeholder="패스워드"
             />
-            <button
+            {/* <button
               type="button"
               onClick={() => (passwordRef.current.type = "password")}
             >
@@ -37,7 +50,7 @@ const LoginPage = () => {
               onClick={() => (passwordRef.current.type = "text")}
             >
               비밀번호 보이게
-            </button>
+            </button> */}
           </div>
 
           <button type="submit" className="search_button form_submit">
