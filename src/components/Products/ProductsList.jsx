77fd2f47803c6b5f8b52f6321653cd493c83b411
kyu <knh3136@naver.com>
2025-01-21 15:17:50 +0900

    제품데이터 카드로 출력

--- src/components/Products/ProductsList.jsx
+++ src/components/Products/ProductsList.jsx
@@ -29,8 +29,17 @@ const ProductsList = () => {
 
       <div className="products_list">
         {error && <em className="form_error">{error}</em>}
-        {products.map((product) => (
-          <ProductCard key={product._id} />
+        {products.map((p) => (
+          <ProductCard
+            key={p._id}
+            id={p._id}
+            image={p.images[0]}
+            title={p.title}
+            price={p.price}
+            rating={p.reviews.rate}
+            ratingCounts={p.reviews.counts}
+            stock={p.stock}
+          />
         ))}
       </div>
     </section>
