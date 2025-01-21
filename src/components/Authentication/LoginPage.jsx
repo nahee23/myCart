5e9f268a98ac53f8b5307c058960cdb8aac2526d
kyu <knh3136@naver.com>
2025-01-21 10:30:37 +0900

    hook form 기능 추가

--- src/components/Authentication/LoginPage.jsx
+++ src/components/Authentication/LoginPage.jsx
@@ -17,10 +17,14 @@ const LoginPage = () => {
   const {
     register,
     handleSubmit,
+    reset,
     formState: { errors },
   } = useForm();
 
-  const submitData = (formData) => console.log(formData);
+  const submitData = (formData) => {
+    console.log(formData);
+    reset();
+  };
 
   return (
     <section className="align_center form_page">
@@ -32,25 +36,40 @@ const LoginPage = () => {
             <input
               // onChange={(e) => setUser({ ...user, email: e.target.value })}
               // value={user.email}
-              {...register("email")}
+              {...register("email", {
+                required: "이메일을 입력해주세요.",
+                pattern: {
+                  value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                  message: "이메일 형식에 맞춰주세요.",
+                },
+              })}
               type="email"
               id="email"
               className="form_text_input"
               placeholder="이메일 입력..."
             />
+            {errors.email && (
+              <em className="form_error">{errors.email.message}</em>
+            )}
           </div>
           <div>
             <label htmlFor="password">Password</label>
             <input
               // onChange={(e) => setUser({ ...user, password: e.target.value })}
               // value={user.password}
-              {...register("password")}
+              {...register("password", {
+                required: "패스워드를 입력해주세요.",
+                minLength: { value: 4, message: "패스워드는 최소 4자 이상" },
+              })}
               type="password"
               //ref={passwordRef}
               id="password"
               className="form_text_input"
               placeholder="패스워드"
             />
+            {errors.password && (
+              <em className="form_error">{errors.password.message}</em>
+            )}
             {/* <button
               type="button"
               onClick={() => (passwordRef.current.type = "password")}
