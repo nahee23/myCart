aafc246b18678d29df07b742b6b8b5cc5fa9c0e8
kyu <knh3136@naver.com>
2025-01-21 14:15:23 +0900

    나브링크적용

--- src/components/Navbar/LinkWithIcon.jsx
+++ src/components/Navbar/LinkWithIcon.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./LinkWithIcon.css";
+import { NavLink } from "react-router-dom";
 
 const LinkWithIcon = ({ title, link, emoji }) => {
   return (
-    <a href={link} className="align_center">
+    //NavLink는 자동으로 active 클래스 추가됨(네브바에서 사용), href => to
+    <NavLink to={link} className="align_center">
       {title} <img src={emoji} alt="" className="link_emoji" />
-    </a>
+    </NavLink>
   );
 };
 
