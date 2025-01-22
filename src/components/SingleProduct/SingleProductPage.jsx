36f0c73bab7307641844321542b321104478fb2b
kyu <knh3136@naver.com>
2025-01-22 15:25:50 +0900

    Quantity

--- src/components/SingleProduct/SingleProductPage.jsx
+++ src/components/SingleProduct/SingleProductPage.jsx
@@ -10,7 +10,8 @@ const SingleProductPage = () => {
   const { id } = useParams(); //주소변수 id를 받음.
   //console.log(id);
   const { data: product, error, isLoading } = useData(`products/${id}`);
-  //console.log(product);
+  console.log(product);
+  const [quantity, setQuantity] = useState(1);
   return (
     <section className="align_center single_product">
       {error && <em className="form_error">{error}</em>}
@@ -46,7 +47,11 @@ const SingleProductPage = () => {
 
             <h2 className="quantity_title">구매개수:</h2>
             <div className="align_center quantity_input">
-              <QuantityInput />
+              <QuantityInput
+                quantity={quantity}
+                setQuantity={setQuantity}
+                stock={product.stock}
+              />
             </div>
 
             <button className="search_button add_cart">장바구니 추가</button>
