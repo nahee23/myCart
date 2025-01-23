89e87c2ab1952665e24f42ca5baf1a3eb3d04519
kyu <knh3136@naver.com>
2025-01-23 10:24:51 +0900

    가입하기 확인

--- src/components/Authentication/SignupPage.jsx
+++ src/components/Authentication/SignupPage.jsx
@@ -2,9 +2,11 @@ import React, { useState } from "react";
 import "./SignupPage.css";
 import user from "../../assets/user.webp";
 import { useForm } from "react-hook-form";
+import { signup } from "../../services/userServices";
 
 const SignupPage = () => {
   const [profilePic, setProfilePic] = useState(null);
+  const [formError, setFormError] = useState("");
   console.log(profilePic);
   const {
     register,
@@ -14,8 +16,12 @@ const SignupPage = () => {
     formState: { errors },
   } = useForm();
 
-  const submitData = (formData) => {
-    console.log(formData);
+  const submitData = async (formData) => {
+    try {
+      await signup(formData, profilePic);
+    } catch (error) {
+      setFormError(error.response.data.message);
+    }
     reset();
   };
   return (
@@ -138,7 +144,7 @@ const SignupPage = () => {
             )}
           </div>
         </div>
-
+        {formError && <em className="form_error">{formError}</em>}
         <button className="search_button form_submit" type="submit">
           Submit
         </button>
