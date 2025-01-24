ba8fdd4aaf15369503324c036c1b06feceb62be8
kyu <knh3136@naver.com>
2025-01-24 10:06:42 +0900

    카트 금액 계산

--- src/components/Cart/CartPage.jsx
+++ src/components/Cart/CartPage.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./CartPage.css";
 import remove from "../../assets/remove.png";
 import user from "../../assets/user.webp";
@@ -6,6 +6,17 @@ import Table from "../Common/Table";
 import QuantityInput from "../SingleProduct/QuantityInput";
 
 const CartPage = ({ cart }) => {
+  //console.log(cart);
+  const [subTotal, setSubTotal] = useState(0);
+
+  useEffect(() => {
+    let total = 0;
+    cart.forEach((item) => {
+      total += item.product.price * item.quantity;
+    });
+    setSubTotal(total);
+  }, [cart]);
+
   return (
     <section className="align_center cart_page">
       <div className="align_center user_info">
@@ -18,21 +29,23 @@ const CartPage = ({ cart }) => {
 
       <Table headings={["상품", "가격", "구매수량", "총 금액", "상품삭제"]}>
         <tbody>
-          <tr>
-            <td>iPhone 14</td>
-            <td>1200,000 원</td>
-            <td className="align_center table_quantity_input">
-              <QuantityInput />
-            </td>
-            <td>1200,000 원</td>
-            <td>
-              <img
-                src={remove}
-                alt="remove icon"
-                className="cart_remove_icon"
-              />
-            </td>
-          </tr>
+          {cart.map(({ product, quantity }) => (
+            <tr key={product._id}>
+              <td>{product.title}</td>
+              <td>{product.price.toLocaleString("ko-KR")} 원</td>
+              <td className="align_center table_quantity_input">
+                <QuantityInput quantity={quantity} stock={product.stock} />
+              </td>
+              <td>{(quantity * product.price).toLocaleString("ko-KR")} 원</td>
+              <td>
+                <img
+                  src={remove}
+                  alt="remove icon"
+                  className="cart_remove_icon"
+                />
+              </td>
+            </tr>
+          ))}
         </tbody>
       </Table>
 
@@ -40,15 +53,15 @@ const CartPage = ({ cart }) => {
         <tbody>
           <tr>
             <td>총 금액</td>
-            <td>1200,000 원</td>
+            <td>{subTotal.toLocaleString("ko-KR")} 원</td>
           </tr>
           <tr>
             <td>배송비</td>
-            <td>5,000 원</td>
+            <td>3,000 원</td>
           </tr>
           <tr className="cart_bill_final">
             <td>결재금액</td>
-            <td>1205,000 원</td>
+            <td>{(subTotal + 3000).toLocaleString("ko-KR")} 원</td>
           </tr>
         </tbody>
       </table>
