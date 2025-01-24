4dcec446d3c699bb5d35df4585eebc8488f8d5cc
kyu <knh3136@naver.com>
2025-01-24 14:33:24 +0900

    로그인한 유저만 장바구니

--- src/components/Products/ProductCard.jsx
+++ src/components/Products/ProductCard.jsx
@@ -4,9 +4,11 @@ import star from "../../assets/white-star.png";
 import basket from "../../assets/basket.png";
 import { NavLink } from "react-router-dom";
 import CartContext from "../../contexts/CartContext";
+import UserContext from "../../contexts/UserContext";
 
 const ProductCard = ({ product }) => {
   const { addToCart } = useContext(CartContext);
+  const user = useContext(UserContext);
   return (
     <article className="product_card">
       <div className="product_image">
@@ -29,7 +31,7 @@ const ProductCard = ({ product }) => {
             <p className="product_review_count">{product?.reviews.counts}</p>
           </div>
           {/* 제품의 재고수가 1개 이상일때만 장바구니 가능 */}
-          {product?.stock > 0 && (
+          {user && product?.stock > 0 && (
             <button
               className="add_to_cart"
               onClick={() => addToCart(product, 1)}
--- src/components/SingleProduct/SingleProductPage.jsx
+++ src/components/SingleProduct/SingleProductPage.jsx
@@ -5,9 +5,11 @@ import { useParams } from "react-router-dom";
 import useData from "../../Hook/useData";
 import Loader from "../Common/Loader";
 import CartContext from "../../contexts/CartContext";
+import UserContext from "../../contexts/UserContext";
 
 const SingleProductPage = () => {
   const { addToCart } = useContext(CartContext);
+  const user = useContext(UserContext);
   const [selectedImage, setSelectedImage] = useState(0);
   const { id } = useParams(); //주소변수 id를 받음.
   //console.log(id);
@@ -46,23 +48,26 @@ const SingleProductPage = () => {
             <p className="single_product_price">
               ￦ {product.price.toLocaleString("ko-KR")} 원
             </p>
+            {user && (
+              <>
+                <h2 className="quantity_title">구매개수:</h2>
+                <div className="align_center quantity_input">
+                  <QuantityInput
+                    quantity={quantity}
+                    setQuantity={setQuantity}
+                    stock={product.stock}
+                    cartPage={false}
+                  />
+                </div>
 
-            <h2 className="quantity_title">구매개수:</h2>
-            <div className="align_center quantity_input">
-              <QuantityInput
-                quantity={quantity}
-                setQuantity={setQuantity}
-                stock={product.stock}
-                cartPage={false}
-              />
-            </div>
-
-            <button
-              onClick={() => addToCart(product, quantity)}
-              className="search_button add_cart"
-            >
-              장바구니 추가
-            </button>
+                <button
+                  onClick={() => addToCart(product, quantity)}
+                  className="search_button add_cart"
+                >
+                  장바구니 추가
+                </button>
+              </>
+            )}
           </div>
         </>
       )}
