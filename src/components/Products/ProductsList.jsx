eb38254d014a1c48ba5f3d4663ab206154f2ebbd
kyu <knh3136@naver.com>
2025-01-31 10:45:55 +0900

    정렬

--- src/components/Products/ProductsList.jsx
+++ src/components/Products/ProductsList.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./ProductsList.css";
 import ProductCard from "./ProductCard";
 import useData from "../../Hook/useData";
@@ -7,6 +7,9 @@ import { useSearchParams } from "react-router-dom";
 import Pagination from "../Common/Pagination";
 
 const ProductsList = () => {
+  const [sortBy, setSortBy] = useState("");
+  //console.log("선택 정렬:" + sortBy);
+  const [sortedProducts, setSortedProducts] = useState([]);
   const [search, setSearch] = useSearchParams(); //요청주소 뒤의 쿼리스트링
   const category = search.get("category"); //category=값
   const page = search.get("page"); // 몇번째 페이지
@@ -25,11 +28,39 @@ const ProductsList = () => {
     setSearch({ ...currentParams, page: page });
   };
 
+  //데이터를 선택정렬방법으로 정렬하기
+  useEffect(() => {
+    if (data && data.products) {
+      const products = [...data.products]; //제품들을 복사
+
+      if (sortBy === "price desc") {
+        setSortedProducts(products.sort((a, b) => b.price - a.price));
+      } else if (sortBy === "price asc") {
+        setSortedProducts(products.sort((a, b) => a.price - b.price));
+      } else if (sortBy === "rate desc") {
+        setSortedProducts(
+          products.sort((a, b) => b.reviews.rate - a.reviews.rate)
+        );
+      } else if (sortBy === "rate asc") {
+        setSortedProducts(
+          products.sort((a, b) => a.reviews.rate - b.reviews.rate)
+        );
+      } else {
+        setSortedProducts(products);
+      }
+    }
+  }, [sortBy, data]);
+
   return (
     <section className="products_list_section">
       <header className="align_center products_list_header">
         <h2>상품목록</h2>
-        <select name="sort" id="" className="products_sorting">
+        <select
+          name="sort"
+          id=""
+          className="products_sorting"
+          onChange={(e) => setSortBy(e.target.value)}
+        >
           <option value="">정렬방법</option>
           <option value="price desc">가격높은순</option>
           <option value="price asc">가격낮은순</option>
@@ -42,9 +73,9 @@ const ProductsList = () => {
         {error && <em className="form_error">{error}</em>}
         {isLoading && skeletons.map((n) => <ProductCardSkeleton key={n} />)}
 
-        {data.products &&
+        {sortedProducts &&
           !isLoading &&
-          data.products.map((product) => (
+          sortedProducts.map((product) => (
             <ProductCard key={product._id} product={product} />
           ))}
         {/* <button onClick={() => handlePageChange(2)}>페이지 2</button> */}
